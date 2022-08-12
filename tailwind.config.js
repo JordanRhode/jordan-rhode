d902d897fbaa12f46bc1d595721137e2c690a804
Jordan Rhode <2262659+JordanRhode@users.noreply.github.com>
2022-08-12 12:57:08 -0500

    Create react app with typescript and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+		fontFamily: {
+			sans: ['Roboto', 'Arial', 'system-ui']
+		},
+    extend: {},
+  },
+  plugins: [],
+}
