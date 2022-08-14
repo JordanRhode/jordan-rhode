424aa02ab0b303686f25311a5ee9f46135596898
Jordan Rhode <2262659+JordanRhode@users.noreply.github.com>
2022-08-14 14:18:59 -0500

    add styles to nav

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,12 +5,12 @@ export type NavigationItem = {
 
 export function Navigation({ items }: { items: NavigationItem[] }) {
 	return (
-		<>
+		<div className="flex flex-col lg:flex-row lg:items-center gap-2">
 			{items.map((item, index) => (
-				<div key={index}>
-					<a href={item.url}>{item.label}</a>
-				</div>
+				<a key={index} href={item.url} className="text-lg hover:text-gray-600">
+					{item.label}
+				</a>
 			))}
-		</>
+		</div>
 	);
 }
